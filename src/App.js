9db99e817a52c7828946f8b02b87762187f91ee7
julu1994 <mrjewel837@gmail.com>
2022-07-29 12:03:50 +0200

    initializing the routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/HomePage/home";
+import Shop from "./Pages/ShopPage/shop";
+import Navbar from "./Components/Navbar/navbar";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello world</h1>
-        </div>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/shop" element={<Shop />} />
+            </Routes>
+        </Router>
     );
 }
 
