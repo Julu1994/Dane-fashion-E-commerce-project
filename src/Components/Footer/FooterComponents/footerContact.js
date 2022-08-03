86e691d1556f574f79438336b2ec341619a691c5
julu1994 <mrjewel837@gmail.com>
2022-08-03 03:15:57 +0200

    working on the footer contact

---
+++ src/Components/Footer/FooterComponents/footerContact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FooterContact = () => {
+    return (
+        <div>
+            <h3>Contact Info</h3>
+            <ul>
+                <li>Kongens nytorv, Copenhagen, Denmark</li>
+                <li>info@danefashion.dk</li>
+                <li>+4500123456</li>
+            </ul>
+            <h4> WE ACCEPT</h4>
+        </div>
+    );
+};
+
+export default FooterContact;
