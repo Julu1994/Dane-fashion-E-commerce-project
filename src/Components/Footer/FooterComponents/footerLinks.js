4673664847a5a9efd59cab2449fa3e5e5802cfb6
julu1994 <mrjewel837@gmail.com>
2022-08-03 02:31:08 +0200

    done working on footer link section

--- src/Components/Footer/FooterComponents/footerLinks.js
+++ src/Components/Footer/FooterComponents/footerLinks.js
@@ -5,21 +5,31 @@ const FooterLinks = () => {
     return (
         <div className="links">
             <h4 className="links-header">Useful Links</h4>
-            <ul>
-                <li>
-                    <a href="#">About Us</a>
+            <ul className="links-list">
+                <li className="links-item">
+                    <a className="links-address" href="/contact">
+                        About Us
+                    </a>
                 </li>
-                <li>
-                    <a href="#">FAQ</a>
+                <li className="links-item">
+                    <a className="links-address" href="/contact">
+                        FAQ
+                    </a>
                 </li>
-                <li>
-                    <a href="#">Location</a>
+                <li className="links-item">
+                    <a className="links-address" href="/contact">
+                        Location
+                    </a>
                 </li>
-                <li>
-                    <a href="#">Partners</a>
+                <li className="links-item">
+                    <a className="links-address" href="/contact">
+                        Partners
+                    </a>
                 </li>
-                <li>
-                    <a href="#">Contact</a>
+                <li className="links-item">
+                    <a className="links-address" href="/contact">
+                        Contact
+                    </a>
                 </li>
             </ul>
         </div>
