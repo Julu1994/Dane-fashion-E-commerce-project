b074a61c76fb45235d5eab53f03283067a0b42be
julu1994 <mrjewel837@gmail.com>
2022-08-03 14:27:04 +0200

    organizig and designing all the components of footer

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,3 +1,4 @@
+import "./footer.scss";
 import React from "react";
 import FooterContact from "./FooterComponents/footerContact";
 import FooterLinks from "./FooterComponents/footerLinks";
@@ -5,25 +6,27 @@ import FooterSocial from "./FooterComponents/footerSocial";
 
 const Footer = () => {
     return (
-        <div>
-            <FooterSocial />
-            <FooterLinks
-                first={"Useful Links"}
-                second={"About Us"}
-                third={"Location"}
-                fourth={"Partners"}
-                fifth={"FAQ"}
-                sixth={"Contact"}
-            />
-            <FooterLinks
-                first={"Catagory"}
-                second={"Men"}
-                third={"Woman"}
-                fourth={"Kids"}
-                fifth={"New Arrivals"}
-                sixth={"Popular Now"}
-            />
-            <FooterContact />
+        <div className="footer">
+            <div className="footer-main">
+                <FooterSocial />
+                <FooterLinks
+                    first={"Useful Links"}
+                    second={"About Us"}
+                    third={"Location"}
+                    fourth={"Partners"}
+                    fifth={"FAQ"}
+                    sixth={"Contact"}
+                />
+                <FooterLinks
+                    first={"Catagory"}
+                    second={"Men"}
+                    third={"Woman"}
+                    fourth={"Kids"}
+                    fifth={"New Arrivals"}
+                    sixth={"Popular Now"}
+                />
+                <FooterContact />
+            </div>
         </div>
     );
 };
