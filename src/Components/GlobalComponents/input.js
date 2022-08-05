194a4bfd1286f70aadf92b5e3b017749a1e1e5dd
julu1994 <mrjewel837@gmail.com>
2022-08-05 14:58:30 +0200

    making a input fiels component for reusing

--- src/Components/GlobalComponents/input.js
+++ src/Components/GlobalComponents/input.js
@@ -1,1 +1,13 @@
+import "./input.scss";
+import React from "react";
 
+const Input = ({}) => {
+    return (
+        <div className="input">
+            <input className="input-field" type="text" />
+            <label>{props}</label>
+        </div>
+    );
+};
+
+export default Input;
