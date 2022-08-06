7372916253c9ba64c22cd55e85720a37f67fa230
julu1994 <mrjewel837@gmail.com>
2022-08-06 23:17:33 +0200

    getting started with Firebase

---
+++ src/Firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKCHy2iiFan_4jvSdpbSAc0W7mbwwcvqY",
+    authDomain: "danedotcom-88638.firebaseapp.com",
+    projectId: "danedotcom-88638",
+    storageBucket: "danedotcom-88638.appspot.com",
+    messagingSenderId: "150357402447",
+    appId: "1:150357402447:web:3cdce1355bc5731accdb3a",
+};
+
+const app = initializeApp(firebaseConfig);
