2196191b0e8d15bc72e150b1c6c6787049756300
julu1994 <mrjewel837@gmail.com>
2022-08-07 12:53:44 +0200

    working on login user with email and password

--- src/Pages/LoginPage/login.js
+++ src/Pages/LoginPage/login.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.scss";
 import Input from "../../Components/GlobalComponents/input";
 import { GoogleButton } from "../../Components/GlobalComponents/button";
 import Button from "../../Components/GlobalComponents/button";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const getEmail = (event) => {
+        setEmail(event.target.value);
+    };
+    const getPassword = (event) => {
+        setPassword(event.target.value);
+    };
+    const userAuthenticate = (event) => {
+        event.preventDefault();
+        console.log(email, password);
+    };
     return (
         <div className="login">
             <div className="login-main">
                 <div className="login-info">
                     <h1 className="login-header">Login</h1>
-                    <form>
-                        <Input text={"Email"} type={"text"} />
-                        <Input text={"Password"} type={"password"} />
-                        <Button text="Login" color={"bg-pink"} />
+                    <form onSubmit={userAuthenticate}>
+                        <Input
+                            text={"Email"}
+                            type={"text"}
+                            onchange={getEmail}
+                        />
+                        <Input
+                            text={"Password"}
+                            type={"password"}
+                            onchange={getPassword}
+                        />
+                        <Button
+                            text="Login"
+                            color={"bg-pink"}
+                            type={"submit"}
+                        />
                     </form>
                     <h4 className="login-divider">or</h4>
                     <GoogleButton text={"LOGIN WITH GOOGLE"} />
