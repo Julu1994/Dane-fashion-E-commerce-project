3a760d9bd0e0ba5409c4b036520eaad781848b80
julu1994 <mrjewel837@gmail.com>
2022-08-08 01:24:35 +0200

    adding reseting password from firebase

--- src/Pages/PasswordReset/reset.js
+++ src/Pages/PasswordReset/reset.js
@@ -1,16 +1,36 @@
 import "./reset.scss";
-import React from "react";
+import React, { useState } from "react";
 import Input from "../../Components/GlobalComponents/input";
 import Button from "../../Components/GlobalComponents/button";
+import { auth } from "../../Firebase/config";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { ToastContainer, toast } from "react-toastify";
 
 export const Reset = () => {
+    const [email, setEmail] = useState("");
+
+    const getEmail = (event) => {
+        setEmail(event.target.value);
+    };
+    const passwordReset = (event) => {
+        event.preventDefault();
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                toast("A reset link has been sent to your email");
+            })
+            .catch((error) => {
+                toast.error(error.message);
+            });
+    };
     return (
         <div className="reset">
+            <ToastContainer />
             <div className="reset-main">
                 <h4>Reset Your Password</h4>
-                <Input text={"Email"} type={"text"} />
-                <Input text={"New Password"} type={"password"} />
-                <Button text="Submit" color={"bg-pink"} />
+                <form onSubmit={passwordReset}>
+                    <Input text={"Email"} type={"text"} onchange={getEmail} />
+                    <Button text="Submit" color={"bg-pink"} type={"submit"} />
+                </form>
             </div>
         </div>
     );
