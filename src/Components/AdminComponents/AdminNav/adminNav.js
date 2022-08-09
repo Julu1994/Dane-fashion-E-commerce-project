d18e630dbf71aeb3bbc8625e9ec6855c5281be93
julu1994 <mrjewel837@gmail.com>
2022-08-10 01:20:09 +0200

    done styling admin nav

--- src/Components/AdminComponents/AdminNav/adminNav.js
+++ src/Components/AdminComponents/AdminNav/adminNav.js
@@ -1,6 +1,9 @@
 import "./adminNav.scss";
 import React from "react";
 import { Link } from "react-router-dom";
+import { IoHome, IoLogoCodepen } from "react-icons/io5";
+import { BsMinecart } from "react-icons/bs";
+import { MdManageAccounts } from "react-icons/md";
 
 const AdminNav = () => {
     return (
@@ -8,21 +11,25 @@ const AdminNav = () => {
             <ul className="adminNav-list">
                 <li className="adminNav-item">
                     <Link className="adminNav-link" to="/">
+                        <IoHome className="adminNav-icon" size={25} />
                         Home
                     </Link>
                 </li>
                 <li className="adminNav-item">
                     <Link className="adminNav-link" to="/">
+                        <BsMinecart className="adminNav-icon" size={25} />
                         View Porducts
                     </Link>
                 </li>
                 <li className="adminNav-item">
                     <Link className="adminNav-link" to="/">
+                        <IoLogoCodepen className="adminNav-icon" size={25} />
                         Post Products
                     </Link>
                 </li>
                 <li className="adminNav-item">
                     <Link className="adminNav-link" to="/">
+                        <MdManageAccounts className="adminNav-icon" size={25} />
                         Manage Orders
                     </Link>
                 </li>
