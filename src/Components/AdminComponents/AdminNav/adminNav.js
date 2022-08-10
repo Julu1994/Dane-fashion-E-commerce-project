df978768a292519a8cf9e05226950c81940b51b0
julu1994 <mrjewel837@gmail.com>
2022-08-10 02:45:03 +0200

    fixing admin nav links

--- src/Components/AdminComponents/AdminNav/adminNav.js
+++ src/Components/AdminComponents/AdminNav/adminNav.js
@@ -10,25 +10,27 @@ const AdminNav = () => {
         <div className="adminNav">
             <ul className="adminNav-list">
                 <li className="adminNav-item">
-                    <Link className="adminNav-link" to="/">
+                    <Link className="adminNav-link" to="/admin/home">
                         <IoHome className="adminNav-icon" size={25} />
                         Home
                     </Link>
                 </li>
                 <li className="adminNav-item">
-                    <Link className="adminNav-link" to="/">
+                    <Link className="adminNav-link" to="/admin/view-products">
                         <BsMinecart className="adminNav-icon" size={25} />
                         View Porducts
                     </Link>
                 </li>
                 <li className="adminNav-item">
-                    <Link className="adminNav-link" to="/">
+                    <Link className="adminNav-link" to="/admin/post-products">
                         <IoLogoCodepen className="adminNav-icon" size={25} />
                         Post Products
                     </Link>
                 </li>
                 <li className="adminNav-item">
-                    <Link className="adminNav-link" to="/">
+                    <Link
+                        className="adminNav-link"
+                        to="/admin/order-management">
                         <MdManageAccounts className="adminNav-icon" size={25} />
                         Manage Orders
                     </Link>
--- src/Components/AdminComponents/ViewProducts/viewProducts.js
+++ src/Components/AdminComponents/ViewProducts/viewProducts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./viewProducts.scss";
 
 const ViewProducts = () => {
     return <div className="view"> ViewProducts</div>;
