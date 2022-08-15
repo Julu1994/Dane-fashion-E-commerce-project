5d6df7a33cdc92cef7d9c825fbc1430bf3ac9345
julu1994 <mrjewel837@gmail.com>
2022-08-15 16:19:03 +0200

    getting started with product cards componets

---
+++ src/Components/Products/productCard.js
@@ -0,0 +1,8 @@
+import "./productCard.scss";
+import React from "react";
+
+const ProductCard = () => {
+    return <div className="card"> ProductCard</div>;
+};
+
+export default ProductCard;
--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../../Components/Banner/banner";
+import ProductCard from "../../Components/Products/productCard";
 
 import "./home.scss";
 
@@ -7,6 +8,7 @@ function Home() {
     return (
         <div>
             <Banner />
+            <ProductCard />
             <div className="imgmodel">Home</div>
         </div>
     );
