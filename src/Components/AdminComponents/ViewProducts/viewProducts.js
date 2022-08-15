cff553867013fc8454ee7cc0aab86076b6207ff2
julu1994 <mrjewel837@gmail.com>
2022-08-15 03:06:02 +0200

    letting admin to delete products from database

--- src/Components/AdminComponents/ViewProducts/viewProducts.js
+++ src/Components/AdminComponents/ViewProducts/viewProducts.js
@@ -3,6 +3,10 @@ import "./viewProducts.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductsData } from "../../../Redux/actionCreators";
 import { MdOutlineDelete, MdOutlineModeEditOutline } from "react-icons/md";
+import { database, storage } from "../../../Firebase/config";
+import { deleteDoc, doc } from "firebase/firestore";
+import { toast } from "react-toastify";
+import { deleteObject, ref } from "firebase/storage";
 
 const ViewProducts = () => {
     const dispatch = useDispatch();
@@ -12,43 +16,61 @@ const ViewProducts = () => {
         dispatch(getProductsData());
     }, []);
 
+    const deleteHandler = async (id, url) => {
+        try {
+            await deleteDoc(doc(database, "shopProducts", id));
+            const reference = ref(storage, url);
+            await deleteObject(reference);
+            toast("The product has been deleted");
+        } catch (error) {
+            toast(error.message);
+        }
+    };
+
     return (
         <div className="view">
             <table className="view-table">
-                <tr className="view-main">
-                    <th>No.</th>
-                    <th>Image</th>
-                    <th>Name</th>
-                    <th>Catagory</th>
-                    <th>Type</th>
-                    <th>Price</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-                {products.map((item, index) => {
-                    return (
-                        <tr className="view-row">
-                            <th> {index + 1}</th>
-                            <th>
-                                <img
-                                    className="view-img"
-                                    src={item.imgUrl}
-                                    alt="productimg"
-                                />
-                            </th>
-                            <th>{item.name}</th>
-                            <th>{item.catagory}</th>
-                            <th>{item.type}</th>
-                            <th>{item.price}</th>
-                            <th>
-                                <MdOutlineDelete size={25} />
-                            </th>
-                            <th>
-                                <MdOutlineModeEditOutline size={25} />
-                            </th>
-                        </tr>
-                    );
-                })}
+                <tbody>
+                    <tr className="view-main">
+                        <th>No.</th>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Catagory</th>
+                        <th>Type</th>
+                        <th>Price</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    {products.map((item, index) => {
+                        return (
+                            <tr className="view-row" key={item.id}>
+                                <td> {index + 1}</td>
+                                <td>
+                                    <img
+                                        className="view-img"
+                                        src={item.imgUrl}
+                                        alt="productimg"
+                                    />
+                                </td>
+                                <td>{item.name}</td>
+                                <td>{item.catagory}</td>
+                                <td>{item.type}</td>
+                                <td>{item.price}</td>
+                                <td>
+                                    <MdOutlineDelete
+                                        size={25}
+                                        onClick={() => {
+                                            deleteHandler(item.id, item.url);
+                                        }}
+                                    />
+                                </td>
+                                <td>
+                                    <MdOutlineModeEditOutline size={25} />
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
             </table>
         </div>
     );
