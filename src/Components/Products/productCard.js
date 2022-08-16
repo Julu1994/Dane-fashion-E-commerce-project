611e4dc1c41c8ce60406033a6523745f36df1cc7
julu1994 <mrjewel837@gmail.com>
2022-08-16 04:22:56 +0200

    working on product card

--- src/Components/Products/productCard.js
+++ src/Components/Products/productCard.js
@@ -1,8 +1,15 @@
 import "./productCard.scss";
 import React from "react";
 
-const ProductCard = () => {
-    return <div className="card"> ProductCard</div>;
+const ProductCard = (props) => {
+    const { productName, productImg, price } = props;
+    return (
+        <div className="card">
+            <img className="card-productimage" src={productImg} alt="product" />
+            <h4 className="card-header">{productName}</h4>
+            <button className="card-btn"> {price} </button>
+        </div>
+    );
 };
 
 export default ProductCard;
--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Banner from "../../Components/Banner/banner";
 import ProductCard from "../../Components/Products/productCard";
+import { getProductsData } from "../../Redux/actionCreators";
 
 import "./home.scss";
 
 function Home() {
+    const dispatch = useDispatch();
+    const productsData = useSelector((state) => state.products.item);
+    const popularProducts = productsData.filter(
+        (item) => item.catagory === "popular"
+    );
+
+    useEffect(() => {
+        dispatch(getProductsData());
+    }, [dispatch]);
     return (
         <div>
             <Banner />
-            <ProductCard />
-            <div className="imgmodel">Home</div>
+            <div>
+                {popularProducts.map((item) => {
+                    return (
+                        <ProductCard
+                            productName={item.name}
+                            productImg={item.imgUrl}
+                            price={item.price}
+                        />
+                    );
+                })}
+            </div>
         </div>
     );
 }
