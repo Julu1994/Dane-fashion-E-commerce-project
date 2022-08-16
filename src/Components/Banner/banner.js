0a77af387363a5fadfd8e233dea98cb85dc35bf2
julu1994 <mrjewel837@gmail.com>
2022-08-16 18:36:19 +0200

    setting up test branch

--- src/Components/Banner/banner.js
+++ src/Components/Banner/banner.js
@@ -4,16 +4,22 @@ import theImage from "../../Resources/model4.png";
 
 const Banner = () => {
     return (
-        <div className="banner">
-            <div className="banner-content">
-                <h1 className="banner-content-main">Fashion</h1>
-                <h6 className="banner-content-title">
-                    LIFE'S TOO SHORT TO WEAR BORING CLOTHES
-                </h6>
-                <button className="banner-content-btn">Explore</button>
-            </div>
-            <div className="banner-right">
-                <img src={theImage} alt="Banner pic" className="banner-img" />
+        <div className="bannerParent">
+            <div className="banner">
+                <div className="banner-content">
+                    <h1 className="banner-content-main">Fashion</h1>
+                    <h6 className="banner-content-title">
+                        LIFE'S TOO SHORT TO WEAR BORING CLOTHES
+                    </h6>
+                    <button className="banner-content-btn">Explore</button>
+                </div>
+                <div className="banner-right">
+                    <img
+                        src={theImage}
+                        alt="Banner pic"
+                        className="banner-img"
+                    />
+                </div>
             </div>
         </div>
     );
