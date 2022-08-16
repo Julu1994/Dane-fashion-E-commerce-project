817ae093f8bcca797c0f8366470daf78b0ead4ee
julu1994 <mrjewel837@gmail.com>
2022-08-16 18:45:44 +0200

    fxing banner

--- src/Components/Banner/banner.js
+++ src/Components/Banner/banner.js
@@ -4,22 +4,16 @@ import theImage from "../../Resources/model4.png";
 
 const Banner = () => {
     return (
-        <div className="bannerParent">
-            <div className="banner">
-                <div className="banner-content">
-                    <h1 className="banner-content-main">Fashion</h1>
-                    <h6 className="banner-content-title">
-                        LIFE'S TOO SHORT TO WEAR BORING CLOTHES
-                    </h6>
-                    <button className="banner-content-btn">Explore</button>
-                </div>
-                <div className="banner-right">
-                    <img
-                        src={theImage}
-                        alt="Banner pic"
-                        className="banner-img"
-                    />
-                </div>
+        <div className="banner">
+            <div className="banner-content">
+                <h1 className="banner-content-main">Fashion</h1>
+                <h6 className="banner-content-title">
+                    LIFE'S TOO SHORT TO WEAR BORING CLOTHES
+                </h6>
+                <button className="banner-content-btn">Explore</button>
+            </div>
+            <div className="banner-right">
+                <img src={theImage} alt="Banner pic" className="banner-img" />
             </div>
         </div>
     );
