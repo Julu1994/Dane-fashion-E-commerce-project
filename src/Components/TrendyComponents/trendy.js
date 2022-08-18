ff938989df1fb726418eff02200e3e2b593db0aa
julu1994 <mrjewel837@gmail.com>
2022-08-18 22:21:04 +0200

    done working on trendy section

--- src/Components/TrendyComponents/trendy.js
+++ src/Components/TrendyComponents/trendy.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "./trendy.scss";
 
 const Trendy = () => {
     return (
         <div className="trendy">
-            <div className="trendy-left">Left</div>
-            <div className="trendy-right">Right</div>
+            <Link to="/lifestyle">
+                <div className="trendy-left">
+                    <h2 className="trendy-left-main">Scandinavian</h2>
+                    <p className="trendy-left-title">
+                        Informal, sophisticated look
+                    </p>
+                    <Link className="trendy-link" to="/lifestyle">
+                        <button className="trendy-link-btn">See more</button>
+                    </Link>
+                </div>
+            </Link>
+            <Link to="/lifestyle">
+                <div className="trendy-right">
+                    <h2 className="trendy-right-main">Sportswear</h2>
+                    <p className="trendy-right-title">Finess, yoga, running</p>
+                    <Link className="trendy-link" to="/lifestyle">
+                        <button className="trendy-link-btn">See more</button>
+                    </Link>
+                </div>
+            </Link>
         </div>
     );
 };
