52819630b22b5e66c2431e0ef69d4b7edf52c614
julu1994 <mrjewel837@gmail.com>
2022-08-19 04:27:56 +0200

    done working on Statics section

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../../Components/Banner/banner";
 import Products from "../../Components/Products/products";
 import SectionHeader from "../../Components/SectionHeader/sectionHeader";
+import Statics from "../../Components/Statics/statics";
 import Trendy from "../../Components/TrendyComponents/trendy";
 import "./home.scss";
 
@@ -24,6 +25,8 @@ const Home = () => {
                 title={"Find your look with soft athleisure!"}
             />
             <Products catagory={"popular"} />
+            <SectionHeader header={"Why Dane.com?"} />
+            <Statics />
         </div>
     );
 };
