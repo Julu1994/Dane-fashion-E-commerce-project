85eab89f44ca514af98a7fac601d0ec3195b4df1
julu1994 <mrjewel837@gmail.com>
2022-08-22 01:30:09 +0200

    styling the cart

--- src/Pages/CartPage/cart.js
+++ src/Pages/CartPage/cart.js
@@ -1,8 +1,30 @@
 import "./cart.scss";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
-    return <div className="cart">Cart</div>;
+    const cartProduct = useSelector((state) => state.cartItem);
+    console.log(cartProduct, "this is cart product... ");
+    return (
+        <div className="cart">
+            Cart..
+            {cartProduct.items?.map((i) => {
+                return (
+                    <div className="item" key={i.id}>
+                        <img
+                            className="item-pic"
+                            src={i.imgUrl}
+                            alt="product"
+                        />
+                        <h3 className="item-name">{i.name}</h3>
+                        <p>{i.totalItemPrice}</p>
+                        <p>Quantity: {i.totalItemQuantity}</p>
+                        <h2></h2>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default Cart;
