7f0ead79eedf5a2e9ec22846b0d665afdf6a23d5
julu1994 <mrjewel837@gmail.com>
2022-08-26 16:12:34 +0200

    start working on connecting all the order steps

--- client/src/Pages/CheckoutPage/checkout.js
+++ client/src/Pages/CheckoutPage/checkout.js
@@ -2,125 +2,128 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
-import StepLabel from "@mui/material/StepLabel";
+import StepButton from "@mui/material/StepButton";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-const steps = [
-    "Select campaign settings",
-    "Create an ad group",
-    "Create an ad",
-];
+const steps = ["Delivery Info", "Confirm the order", "Payment"];
 
 const Checkout = () => {
     const [activeStep, setActiveStep] = React.useState(0);
-    const [skipped, setSkipped] = React.useState(new Set());
+    const [completed, setCompleted] = React.useState({});
 
-    const isStepOptional = (step) => {
-        return step === 1;
+    const totalSteps = () => {
+        return steps.length;
     };
 
-    const isStepSkipped = (step) => {
-        return skipped.has(step);
+    const completedSteps = () => {
+        return Object.keys(completed).length;
+    };
+
+    const isLastStep = () => {
+        return activeStep === totalSteps() - 1;
+    };
+
+    const allStepsCompleted = () => {
+        return completedSteps() === totalSteps();
     };
 
     const handleNext = () => {
-        let newSkipped = skipped;
-        if (isStepSkipped(activeStep)) {
-            newSkipped = new Set(newSkipped.values());
-            newSkipped.delete(activeStep);
-        }
-
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setSkipped(newSkipped);
+        const newActiveStep =
+            isLastStep() && !allStepsCompleted()
+                ? // It's the last step, but not all steps have been completed,
+                  // find the first step that has been completed
+                  steps.findIndex((step, i) => !(i in completed))
+                : activeStep + 1;
+        setActiveStep(newActiveStep);
     };
 
     const handleBack = () => {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
     };
 
-    const handleSkip = () => {
-        if (!isStepOptional(activeStep)) {
-            // You probably want to guard against something like this,
-            // it should never occur unless someone's actively trying to break something.
-            throw new Error("You can't skip a step that isn't optional.");
-        }
+    const handleStep = (step) => () => {
+        setActiveStep(step);
+    };
 
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setSkipped((prevSkipped) => {
-            const newSkipped = new Set(prevSkipped.values());
-            newSkipped.add(activeStep);
-            return newSkipped;
-        });
+    const handleComplete = () => {
+        const newCompleted = completed;
+        newCompleted[activeStep] = true;
+        setCompleted(newCompleted);
+        handleNext();
     };
 
     const handleReset = () => {
         setActiveStep(0);
+        setCompleted({});
     };
 
     return (
         <Box sx={{ width: "100%" }}>
-            <Stepper activeStep={activeStep}>
-                {steps.map((label, index) => {
-                    const stepProps = {};
-                    const labelProps = {};
-                    if (isStepOptional(index)) {
-                        labelProps.optional = (
-                            <Typography variant="caption">Optional</Typography>
-                        );
-                    }
-                    if (isStepSkipped(index)) {
-                        stepProps.completed = false;
-                    }
-                    return (
-                        <Step key={label} {...stepProps}>
-                            <StepLabel {...labelProps}>{label}</StepLabel>
-                        </Step>
-                    );
-                })}
+            <Stepper nonLinear activeStep={activeStep}>
+                {steps.map((label, index) => (
+                    <Step key={label} completed={completed[index]}>
+                        <StepButton color="inherit" onClick={handleStep(index)}>
+                            {label}
+                        </StepButton>
+                    </Step>
+                ))}
             </Stepper>
-            {activeStep === steps.length ? (
-                <React.Fragment>
-                    <Typography sx={{ mt: 2, mb: 1 }}>
-                        All steps completed - you&apos;re finished
-                    </Typography>
-                    <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-                        <Box sx={{ flex: "1 1 auto" }} />
-                        <Button onClick={handleReset}>Reset</Button>
-                    </Box>
-                </React.Fragment>
-            ) : (
-                <React.Fragment>
-                    <Typography sx={{ mt: 2, mb: 1 }}>
-                        Step {activeStep + 1}
-                    </Typography>
-                    <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-                        <Button
-                            color="inherit"
-                            disabled={activeStep === 0}
-                            onClick={handleBack}
-                            sx={{ mr: 1 }}>
-                            Back
-                        </Button>
-                        <Box sx={{ flex: "1 1 auto" }} />
-                        {isStepOptional(activeStep) && (
+            <div>
+                {allStepsCompleted() ? (
+                    <React.Fragment>
+                        <Typography sx={{ mt: 2, mb: 1 }}>
+                            All steps completed - you&apos;re finished
+                        </Typography>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                flexDirection: "row",
+                                pt: 2,
+                            }}>
+                            <Box sx={{ flex: "1 1 auto" }} />
+                            <Button onClick={handleReset}>Reset</Button>
+                        </Box>
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                        <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
+                            Step {activeStep + 1}
+                        </Typography>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                flexDirection: "row",
+                                pt: 2,
+                            }}>
                             <Button
                                 color="inherit"
-                                onClick={handleSkip}
+                                disabled={activeStep === 0}
+                                onClick={handleBack}
                                 sx={{ mr: 1 }}>
-                                Skip
+                                Back
                             </Button>
-                        )}
-
-                        <Button onClick={handleNext}>
-                            {activeStep === steps.length - 1
-                                ? "Finish"
-                                : "Next"}
-                        </Button>
-                    </Box>
-                </React.Fragment>
-            )}
+                            <Box sx={{ flex: "1 1 auto" }} />
+                            {activeStep !== steps.length &&
+                                (completed[activeStep] ? (
+                                    <Typography
+                                        variant="caption"
+                                        sx={{ display: "inline-block" }}>
+                                        Step {activeStep + 1} already completed
+                                    </Typography>
+                                ) : (
+                                    <Button onClick={handleComplete}>
+                                        {completedSteps() === totalSteps() - 1
+                                            ? "Finish"
+                                            : "Complete Step"}
+                                    </Button>
+                                ))}
+                        </Box>
+                    </React.Fragment>
+                )}
+            </div>
         </Box>
     );
 };
+
 export default Checkout;
