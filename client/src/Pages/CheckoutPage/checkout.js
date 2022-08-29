b20cb84d06d038a39048d36caf0f82cf9ce3fa09
julu1994 <mrjewel837@gmail.com>
2022-08-29 04:19:49 +0200

    customizing mui stepper

--- client/src/Pages/CheckoutPage/checkout.js
+++ client/src/Pages/CheckoutPage/checkout.js
@@ -2,75 +2,54 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
-import StepButton from "@mui/material/StepButton";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { StepLabel } from "@mui/material";
 
 const steps = ["Delivery Info", "Confirm the order", "Payment"];
 
 const Checkout = () => {
-    const [activeStep, setActiveStep] = React.useState(0);
-    const [completed, setCompleted] = React.useState({});
+    const [activeStepp, setActiveStepp] = React.useState(0);
 
     const totalSteps = () => {
         return steps.length;
     };
 
-    const completedSteps = () => {
-        return Object.keys(completed).length;
-    };
-
     const isLastStep = () => {
-        return activeStep === totalSteps() - 1;
+        return activeStepp === totalSteps() - 1;
     };
-
-    const allStepsCompleted = () => {
-        return completedSteps() === totalSteps();
+    const iscomplete = () => {
+        return activeStepp === totalSteps();
     };
 
     const handleNext = () => {
-        const newActiveStep =
-            isLastStep() && !allStepsCompleted()
-                ? // It's the last step, but not all steps have been completed,
-                  // find the first step that has been completed
-                  steps.findIndex((step, i) => !(i in completed))
-                : activeStep + 1;
-        setActiveStep(newActiveStep);
+        setActiveStepp((step) => step + 1);
     };
 
     const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-
-    const handleStep = (step) => () => {
-        setActiveStep(step);
-    };
-
-    const handleComplete = () => {
-        const newCompleted = completed;
-        newCompleted[activeStep] = true;
-        setCompleted(newCompleted);
-        handleNext();
+        setActiveStepp((prevActiveStep) => prevActiveStep - 1);
     };
 
     const handleReset = () => {
-        setActiveStep(0);
-        setCompleted({});
+        setActiveStepp(0);
     };
+    if (isLastStep) {
+        console.log(" false value");
+    } else {
+        console.log("true value");
+    }
 
     return (
-        <Box sx={{ width: "100%" }}>
-            <Stepper nonLinear activeStep={activeStep}>
-                {steps.map((label, index) => (
-                    <Step key={label} completed={completed[index]}>
-                        <StepButton color="inherit" onClick={handleStep(index)}>
-                            {label}
-                        </StepButton>
+        <Box sx={{ width: "90%", marginLeft: "5%" }}>
+            <Stepper activeStep={activeStepp} alternativeLabel>
+                {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
                     </Step>
                 ))}
             </Stepper>
             <div>
-                {allStepsCompleted() ? (
+                {iscomplete() ? (
                     <React.Fragment>
                         <Typography sx={{ mt: 2, mb: 1 }}>
                             All steps completed - you&apos;re finished
@@ -88,7 +67,7 @@ const Checkout = () => {
                 ) : (
                     <React.Fragment>
                         <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
-                            Step {activeStep + 1}
+                            Step :
                         </Typography>
                         <Box
                             sx={{
@@ -98,26 +77,13 @@ const Checkout = () => {
                             }}>
                             <Button
                                 color="inherit"
-                                disabled={activeStep === 0}
+                                disabled={activeStepp === 0}
                                 onClick={handleBack}
                                 sx={{ mr: 1 }}>
                                 Back
                             </Button>
                             <Box sx={{ flex: "1 1 auto" }} />
-                            {activeStep !== steps.length &&
-                                (completed[activeStep] ? (
-                                    <Typography
-                                        variant="caption"
-                                        sx={{ display: "inline-block" }}>
-                                        Step {activeStep + 1} already completed
-                                    </Typography>
-                                ) : (
-                                    <Button onClick={handleComplete}>
-                                        {completedSteps() === totalSteps() - 1
-                                            ? "Finish"
-                                            : "Complete Step"}
-                                    </Button>
-                                ))}
+                            <Button onClick={handleNext}>next</Button>
                         </Box>
                     </React.Fragment>
                 )}
