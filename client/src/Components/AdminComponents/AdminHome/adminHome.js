aa429783ce84d2233e5be063ce860d5413d2117d
julu1994 <mrjewel837@gmail.com>
2022-09-02 15:00:07 +0200

    displaying earning to admin home page

--- client/src/Components/AdminComponents/AdminHome/adminHome.js
+++ client/src/Components/AdminComponents/AdminHome/adminHome.js
@@ -1,16 +1,35 @@
 import "./adminHome.scss";
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getOrderData } from "../../../Redux/actionCreator2";
 
 const AdminHome = () => {
-    //const orders = useSelector((state) => state.orderHistory.order);
-    //const earnings = orders.cartItem.map((i) => i.totalAmount);
-    //console.log(orders, "latest ");
+    const orders = useSelector((state) => state.orderHistory.orders);
+    const earnings = orders.map((i) => i.order.cartItem.totalAmount);
+    const totalEarnig = earnings.reduce(
+        (previous, current) => previous + current
+    );
+    const soldItems = orders.map((i) => i.order.cartItem.totalQuantity);
+    const totalSoldItems = soldItems.reduce(
+        (previous, current) => previous + current
+    );
+    const totalOrders = earnings.length;
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getOrderData());
+    }, [dispatch]);
     return (
         <div className="adminHome">
-            <div className="adminHom-child">
-                <h1>Total Earnings: </h1>
-            </div>
+            <h2 className="adminHome-earning">
+                Total Earnings: {totalEarnig}.00$
+            </h2>
+            <h2 className="adminHome-orders">
+                Total Number of Orders: {totalOrders}
+            </h2>
+            <h2 className="adminHome-items">
+                Total Products Sold: {totalSoldItems}
+            </h2>
         </div>
     );
 };
